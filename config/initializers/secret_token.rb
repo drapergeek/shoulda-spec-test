73138287a591fa3f4d21096397c0c636e1ee6ed6
Jason Draper <jason@drapergeek.com>
2012-11-02 10:48:23 -0400

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShouldaSpecTest::Application.config.secret_token = '78925673bfcc61a77a3a0e20428cc84bdc6fee21ec412184df4f94c6ac3377dde21adcf3fb9b2ef65e08f77e49b6e2919f7c486a8352be2b9094301c6d266254'
